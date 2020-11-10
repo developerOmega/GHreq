e39147fcea0d23f652579b86fb77534aed17ef23
developerOmega <developerOmega@developerOmega.com>
2020-11-09 19:45:05 -0600

    Agregar estilos de info component

--- src/screens/Info.js
+++ src/screens/Info.js
@@ -1,29 +1,108 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import IconMC from 'react-native-vector-icons/MaterialCommunityIcons';
+import StyleText from '../styles/Text';
+import StyleView from '../styles/View';
 import Var from '../styles/Var';
 
 const Info = () => {
   return (
-    <View style={styles.center}>
-      <Text style={styles.title}>
-        Este es el Info
-      </Text>
+    <View style={[StyleView.container]}>
+      <View style={styles.container}>
+
+        <Text style={[StyleText.mainTitle, styles.paddingBottom]}>
+          GHReq info
+        </Text>
+        <Text style={[StyleText.secondTitle, styles.paddingBottom]}>
+          Esta es una App móvil creada por Daniel Mendoza (DeveloperOmega), sobre la librería React Native.
+        </Text>
+
+        <Text style={[StyleText.secondTitle, styles.paddingBottom]}>
+          Consume el API de GitHub por lo que se realiza una autenticación Oauth2 en el login; consume
+          los request de usuarios, proyectos y commits; además el usuario tiene la capacidad
+          de crear nuevos repositorios. 
+        </Text>
+
+        <Text style={[StyleText.secondTitle, styles.paddingBottom]}>
+          Para más información consultar mis redes.
+        </Text>
+
+        <View style={styles.iconContainer}>
+          <Icon
+            style={[styles.fs40, {color: Var.colorLinkedin}]}
+            name="linkedin-square"
+          />
+          <Text style={[StyleText.secondTitle, StyleText.fs20, styles.paddingLeft]}>
+            Daniel Mendoza
+          </Text>
+        </View>
+
+        <View style={styles.iconContainer}>
+          <IconMC
+            style={[styles.fs40, {color: Var.colorGmail}]}
+            name="gmail"
+          />
+          <Text style={[StyleText.secondTitle, StyleText.fs20, styles.paddingLeft]}>
+            theskip98@gmail.com 
+          </Text>
+        </View>
+
+        <View style={styles.iconContainer}>
+          <IconMC
+            style={[styles.fs40, {color: Var.colorText}]}
+            name="web"
+          />
+          <Text style={[StyleText.secondTitle, StyleText.secondTitle, styles.paddingLeft]}>
+            http://developeromega.herokuapp.com/
+          </Text>
+        </View>
+
+        <View style={styles.iconContainer}>
+          <Icon
+            style={[styles.fs40, {color: Var.colorTwitter}]}
+            name="twitter-square"
+          />
+          <Text style={[StyleText.secondTitle, StyleText.fs20, styles.paddingLeft]}>
+            @DeveloperOmega
+          </Text>
+        </View>
+
+        <View style={styles.iconContainer}>
+          <Icon
+            style={[styles.fs40, {color: Var.colorFacebook}]}
+            name="facebook-square"
+          />
+          <Text style={[StyleText.secondTitle, StyleText.fs20, styles.paddingLeft]}>
+            @developerOmega
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: "center",
-    alignItems: 'center'
+  paddingBottom: {
+    paddingBottom: 20
   },
-  title: {
-    color: Var.colorText,
-    fontSize: 30,
-    fontWeight: 'bold'
+  paddingLeft: {
+    paddingLeft: 10
+  },
+  fs40: {
+    fontSize: 40
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingBottom: 10
+  },
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5
   }
+
 });
 
 export default Info;
--- src/styles/Var.js
+++ src/styles/Var.js
@@ -2,5 +2,9 @@ export default {
   colorText: '#313131',
   colorGreen: '#28A745',
   colorGray: '#E1E1E1',
-  colorSecond: '#717171'
+  colorSecond: '#717171',
+  colorLinkedin: '#0e76a8',
+  colorGmail:  '#db4a39',
+  colorTwitter: '#00acee',
+  colorFacebook: '#3b5998'
 }
